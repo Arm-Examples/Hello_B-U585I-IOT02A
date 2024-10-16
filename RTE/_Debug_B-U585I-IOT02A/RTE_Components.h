23ee5e059e39fd4ad548a159a8d4f015bbbc3bed
evangelos.ganotopoulos@arm.com <Eve07@Git>
2024-10-16 11:23:58 +0200

    Updated files for reproducable builds

--- RTE/_Debug_B-U585I-IOT02A/RTE_Components.h
+++ RTE/_Debug_B-U585I-IOT02A/RTE_Components.h
@@ -24,47 +24,47 @@
 #define RTE_CMSIS_RTOS2_RTX5_SOURCE     /* CMSIS-RTOS2 Keil RTX5 Source */
 /* Keil::Device:STM32Cube Framework:STM32CubeMX@2.0.0 */
 #define RTE_DEVICE_FRAMEWORK_CUBE_MX
-/* Keil::Device:STM32Cube HAL:Common@1.2.0 */
+/* Keil::Device:STM32Cube HAL:Common@1.3.0 */
 #define RTE_DEVICE_HAL_COMMON
-/* Keil::Device:STM32Cube HAL:Cortex@1.2.0 */
+/* Keil::Device:STM32Cube HAL:Cortex@1.3.0 */
 #define RTE_DEVICE_HAL_CORTEX
-/* Keil::Device:STM32Cube HAL:DMA@1.2.0 */
+/* Keil::Device:STM32Cube HAL:DMA@1.3.0 */
 #define RTE_DEVICE_HAL_DMA
-/* Keil::Device:STM32Cube HAL:Flash@1.2.0 */
+/* Keil::Device:STM32Cube HAL:Flash@1.3.0 */
 #define RTE_DEVICE_HAL_FLASH
-/* Keil::Device:STM32Cube HAL:GPIO@1.2.0 */
+/* Keil::Device:STM32Cube HAL:GPIO@1.3.0 */
 #define RTE_DEVICE_HAL_GPIO
-/* Keil::Device:STM32Cube HAL:I2C@1.2.0 */
+/* Keil::Device:STM32Cube HAL:I2C@1.3.0 */
 #define RTE_DEVICE_HAL_I2C
-/* Keil::Device:STM32Cube HAL:ICACHE@1.2.0 */
+/* Keil::Device:STM32Cube HAL:ICACHE@1.3.0 */
 #define RTE_DEVICE_HAL_ICACHE
-/* Keil::Device:STM32Cube HAL:MDF@1.2.0 */
+/* Keil::Device:STM32Cube HAL:MDF@1.3.0 */
 #define RTE_DEVICE_HAL_MDF
-/* Keil::Device:STM32Cube HAL:OSPI@1.2.0 */
+/* Keil::Device:STM32Cube HAL:OSPI@1.3.0 */
 #define RTE_DEVICE_HAL_OSPI
-/* Keil::Device:STM32Cube HAL:PCD@1.2.0 */
+/* Keil::Device:STM32Cube HAL:PCD@1.3.0 */
 #define RTE_DEVICE_HAL_PCD
-/* Keil::Device:STM32Cube HAL:PWR@1.2.0 */
+/* Keil::Device:STM32Cube HAL:PWR@1.3.0 */
 #define RTE_DEVICE_HAL_PWR
-/* Keil::Device:STM32Cube HAL:RCC@1.2.0 */
+/* Keil::Device:STM32Cube HAL:RCC@1.3.0 */
 #define RTE_DEVICE_HAL_RCC
-/* Keil::Device:STM32Cube HAL:SPI@1.2.0 */
+/* Keil::Device:STM32Cube HAL:SPI@1.3.0 */
 #define RTE_DEVICE_HAL_SPI
-/* Keil::Device:STM32Cube HAL:UART@1.2.0 */
+/* Keil::Device:STM32Cube HAL:UART@1.3.0 */
 #define RTE_DEVICE_HAL_UART
-/* Keil::Device:STM32Cube LL:Common@1.2.0 */
+/* Keil::Device:STM32Cube LL:Common@1.3.0 */
 #define RTE_DEVICE_LL_COMMON
-/* Keil::Device:STM32Cube LL:DLYB@1.2.0 */
+/* Keil::Device:STM32Cube LL:DLYB@1.3.0 */
 #define RTE_DEVICE_LL_DLYB
-/* Keil::Device:STM32Cube LL:GPIO@1.2.0 */
+/* Keil::Device:STM32Cube LL:GPIO@1.3.0 */
 #define RTE_DEVICE_LL_GPIO
-/* Keil::Device:STM32Cube LL:PWR@1.2.0 */
+/* Keil::Device:STM32Cube LL:PWR@1.3.0 */
 #define RTE_DEVICE_LL_PWR
-/* Keil::Device:STM32Cube LL:RCC@1.2.0 */
+/* Keil::Device:STM32Cube LL:RCC@1.3.0 */
 #define RTE_DEVICE_LL_RCC
-/* Keil::Device:STM32Cube LL:UCPD@1.2.0 */
+/* Keil::Device:STM32Cube LL:UCPD@1.3.0 */
 #define RTE_DEVICE_LL_UCPD
-/* Keil::Device:STM32Cube LL:UTILS@1.2.0 */
+/* Keil::Device:STM32Cube LL:UTILS@1.3.0 */
 #define RTE_DEVICE_LL_UTILS
 /* Keil::Device:Startup&Source C@1.0.0 */
 #define RTE_DEVICE_STARTUP_STM32U5XX    /* Device Startup for STM32U5 */
